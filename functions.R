761bce3102c78e9d7728d41c6c289fe29f5cf823
Amyntas <aa21qeqa@idiv.de>
2022-05-14 19:20:29 +0200

    new function does not work

--- functions.R
+++ functions.R
@@ -1,4 +1,70 @@
 retrieve_from_Jexis <- function(id) { 
+  require(tidyverse)
+  require(rvest)
+  require(rJava)
+  require(RSelenium)
+  
+  temp = tempdir()
+  # downloading in folder temp
+  downloadPath <- file.path(temp) %>% stringr::str_replace_all("\\\\", "\\\\\\\\") # dont question it
+  # to save the file in a temp directory
+  fprof <- makeFirefoxProfile(list(browser.download.dir = downloadPath,
+                                   browser.download.folderList = 2L,
+                                   browser.download.manager.showWhenStarting = FALSE,
+                                   browser.helperApps.neverAsk.openFile = "text/csv",
+                                   browser.helperApps.neverAsk.saveToDisk = "text/csv"))
+  
+  # to make firefox headless
+  exCap <- list(firefox_profile = fprof$firefox_profile, 
+                "moz:firefoxOptions" = list(args = list('--headless')))
+  
+  # start the thingy
+  driver <- rsDriver(browser = "firefox", verbose = FALSE
+                     ,extraCapabilities = exCap
+  )
+  remDr<-driver[["client"]]
+  
+  # go to the page of the dataset
+  remDr$navigate(paste0("https://jexis.idiv.de/ddm/data/Showdata/", id))
+  
+  # find and click the dropdown menu
+  webElem1 <- remDr$findElements(using='xpath',
+                                 value= '//*[@id="showData"]')
+  name <- webElem1[[2]]$getElementText()
+  name <- strsplit(name[[1]], 
+                   "\\\\|[^[:print:]]", 
+                   fixed=FALSE)[[1]][1]
+  
+  Sys.sleep(1)
+  # go to the primary data tab
+  webElem1 <- remDr$findElement(using='id', 
+                                value= "primarydata")
+  webElem1$clickElement()
+  
+  Sys.sleep(1)
+  # find and click the dropdown menu
+  webElem1 <- remDr$findElements(using='xpath',
+                                 value= '//*[@id="dropdownMenu1"]')
+  webElem1[[2]]$clickElement()
+  
+  webElem1 <- remDr$findElement(using='xpath',
+                                value= '//*[@ext=".csv"]')
+  webElem1$clickElement()
+  
+  data = read.csv(Sys.glob(paste0(temp, "\\", name, "*.csv")), sep = ";")
+  
+  # closes firefox remote browser (which you don't see if headless)
+  remDr$closeWindow()
+  # kills java
+  system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE,
+         show.output.on.console = FALSE)
+  
+  return(data)
+}
+
+
+# this was my first attempt, now obsolete
+retrieve_from_Jexis_crawler <- function(id) { 
 require(tidyverse)
 require(rvest)
 require(rJava)
@@ -91,3 +157,4 @@ system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE,
 
 return(data)
 }
+
